d799c80a8cc84b3938dd439fd7eec3a2669001d1
Muluken Taye <muluken@berkeley.edu>
2016-11-02 16:59:51 -0700

    completed part 4

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -4,4 +4,13 @@ class PokemonsController < ApplicationController
 		@pokemons.update_attributes!(:trainer => current_trainer)
 		redirect_to :root
 	end
+
+	def damage
+		@pokemons = Pokemon.find(params[:id])
+		@pokemons.update_attributes!(:health => (@pokemons.health - 10))
+		if @pokemons.health < 0
+			@pokemons.destroy
+		end
+		redirect_to trainer_path(current_trainer)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :trainers
   resources :trainers
-  patch '/:id', :to => 'pokemons#capture', :as => 'capture'
+  patch 'capture/:id', :to => 'pokemons#capture', :as => 'capture'
+  patch 'damage/:id', :to => 'pokemons#damage', :as => 'damage'
 end
